4b37d05bd9ca05205f84888a8dceafaac493501e
agarwalsahil0210 <53162729+agarwalsahil0210@users.noreply.github.com>
2019-07-27 19:45:20 +0530

    Add files via upload

---
+++ square_sum_difference.java
@@ -0,0 +1,13 @@
+class square_sum_difference
+{
+    public static void main(String args[])
+    {
+    int s1=0,s2=0,i;
+    for(i=1;i<=100;i++)
+    {
+        s1=s1+(i*i);
+        s2=s2+i;
+    }
+    System.out.println("Difference:"+((s2*s2)-s1));
+}
+}
