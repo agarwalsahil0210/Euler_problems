1793917ec7d0f5470936e6c51d624c2361cad4c6
agarwalsahil0210 <53162729+agarwalsahil0210@users.noreply.github.com>
2019-07-29 16:31:27 +0530

    Add files via upload

---
+++ largest_palindrome_product.java
@@ -0,0 +1,32 @@
+public class largest_palindrome_product
+ {
+
+	public static void main(String[] args)
+	{
+		 int max=-1,num=1;
+			for(int i=999;i>=100;i--)
+			{
+				for(int j=999;j>=100;j--)
+				{
+				     num= (i*j);
+					 int t=num;
+						if (ispalindrome(num) && num>max)
+						max = t;
+				 }
+			}
+			System.out.println("Largest palinmdrome product is :"+max);
+		}
+	       public static boolean ispalindrome(int x) 
+	       {
+	        int rem,rev=0,t=x;
+			while(x>0) 
+			{
+			    rem=x%10;
+				rev=rev*10+rem;	
+				x=x/10;
+			   }
+			return  t==rev;
+
+	}
+
+}
