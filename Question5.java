d9cd6c4976df15340645dd9630a02314822efaad
agarwalsahil0210 <53162729+agarwalsahil0210@users.noreply.github.com>
2019-07-31 19:51:05 +0530

    Add files via upload

---
+++ Question5.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Question5
+{
+    //******************************************************************
+    //*Author: Sahil Agarwal
+    //*Purpose:To find the smallest multiple of all numbers from 1 to 20
+    //******************************************************************
+    public static void main(String args[])
+    {
+        int i,n=2520,t=0;
+        while(true)
+        {
+            for(i=1;i<=20;i++)
+            {
+                if(n%i!=0)
+                break;
+                else if(i==20)
+                {
+                    t=1;
+                }
+            }
+            n++;
+            if(t==1)
+            {
+                System.out.println("Smallest multiple of all the numbers from 1 to 20 is :"+(n-1));
+                break;
+            }
+        }
+    }
+}
