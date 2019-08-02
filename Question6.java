269c2406152fdfa1aafcf7f7819c2c936ab1a4f6
agarwalsahil0210 <53162729+agarwalsahil0210@users.noreply.github.com>
2019-08-02 23:08:17 +0530

    Add files via upload

--- Question6.java
+++ Question6.java
@@ -7,11 +7,8 @@ class Question6
     public static void main(String args[])
     {
     int s1=0,s2=0,i;
-    for(i=1;i<=100;i++)
-    {
-        s1=s1+(i*i);
-        s2=s2+i;
-    }
+    s2=(100*101)/2;
+    s1=(100*101*201)/6;
     System.out.println("Difference:"+((s2*s2)-s1));
 }
 }
